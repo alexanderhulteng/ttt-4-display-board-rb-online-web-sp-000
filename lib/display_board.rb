d6f3945b7927b5f056379bec6c4b22500d324c1a
Alexander Hulteng <alexanderhulteng@gmail.com>
2020-07-16 01:35:50 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,10 @@
-# Define display_board that accepts a board and prints
-# out the current state.
-def display_board
-  puts "   |   |   "
+board = [" "," "," "," "," "," "," "," "," "]
+def display_board(tiles)
+  puts " #{tiles[0]}  |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 end
+
+display_board(tiles)
