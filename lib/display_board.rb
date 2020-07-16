5ac7a0a19f61237659f29fba0ecf9c0ea7163390
Alexander Hulteng <alexanderhulteng@gmail.com>
2020-07-16 01:37:45 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@ board = ["x","x","x"," "," "," "," "," "," "]
 def display_board(tiles)
   puts " #{tiles[0]} | #{tiles[1]} | #{tiles[2]}  "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{tiles[0]} | #{tiles[1]} | #{tiles[2]}  "
   puts "-----------"
   puts "   |   |   "
 end
